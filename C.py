4b6059da0e055246f4c328ca2873bb8bda43d7ca
Tom Trikalinos <tom@ttrikalin-mac.local>
2010-06-26 10:07:39 -0400

    first commit

---
+++ C.py
@@ -0,0 +1,11 @@
+DEFAULT_SAVE_FILENAME = 'savedrecs.bib'
+#SAVE_DIR = 'C:\\Downloads\\'
+#DATA_DIR = 'C:\\Documents and Settings\\TTrikalinos\\Desktop\\test\\corpii\\case1\\citations\\'
+#PUBMED_DATA_LOCATION = 'C:\\Documents and Settings\\TTrikalinos\\Desktop\\test\\corpii\\case1\\'
+SAVE_DIR = '/Users/tom/src/citation_graphs/test/citation_graphs/temp'
+DATA_DIR = '/Users/tom/src/citation_graphs/test/case1/citations'
+PUBMED_DATA_LOCATION = '/Users/tom/src/citation_graphs/test/case1'
+DATA_LIMIT = None
+
+
+#  /Applications/Firefox.app/Contents/MacOS/firefoxbin -ProfileManager
