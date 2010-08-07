d0ce37b219064d5c8054f4b64d510269783aa24d
Tom Trikalinos <tom@ttrikalin-mac.local>
2010-08-07 08:02:50 -0700

    sel_isi.py fixed title comparisons

---
+++ setup_omega3_cvd.py
@@ -0,0 +1,23 @@
+# Give a Pubmed XML and if needed edit the first line to automatically fix it :) 
+# Then give the relative path to the Results folder in which the topic-specific 
+# data will be saved, in a folder that has the PROJECT_NAME
+
+# May be convenient to name the 
+
+import os
+
+FILENAME = "Pubmed.xml"
+REL_RES_PATH = "Results"
+PROJECT_NAME = "omega3_cvd"
+
+from pubmed_functions import * 
+
+# get the xml if not already there 
+# TO DO: automatically fix the filter bug
+#download_from_pubmed(SEARCH, FILENAME, 5000)
+
+# parse the xml into [pmids, titles, years]
+pmids_titles_years = parse_pubmed_xml(os.path.join(REL_RES_PATH, PROJECT_NAME,'Search',FILENAME))
+
+# organize saved files
+prepare_search_files(pmids_titles_years, REL_RES_PATH, PROJECT_NAME)
